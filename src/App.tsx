ee84384d686d35ae0f1a21cf6d66026eb2ee0068
GoodSoldier1P1 <danielamyx859@gmail.com>
2024-02-21 21:43:57 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+// import twistedLogo from './Images/twistedLogo.avif'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Home/Home.tsx
@@ -0,0 +1,33 @@
+const Home = () => {
+    return (
+        <>
+            <div className="twistedStache">
+                <div className="navbar">
+                    <div className="logo"> </div>
+
+                    <div className="navbuttons"> </div>
+
+                    <div className="wspaceNavB" />
+
+                    <div className="wspaceNavT" />
+
+                    <div className="wspaceNavM" />
+
+                </div>
+
+                <div className="heroImg"> </div>
+
+                <div className="services"> </div>
+
+                <div className="wspaceL" />
+
+                <div className="wspaceR" />
+
+                <div className="footer"> </div>
+
+            </div>
+
+        </>
+    )
+}
+export default Home
